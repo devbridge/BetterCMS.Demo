2a780bda830993401504c12a90892ec53154ab90
Simonas Mikulenas <simonas@devbridge.com>
2013-04-10 16:35:42 +0300

    BetterCMS 1.0.0 added. MDF file created.

--- BetterCms.Demo.Web/Global.asax.cs
+++ BetterCms.Demo.Web/Global.asax.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Routing;
+using BetterCms.Core;
+using BetterCms.Core.Environment.Host;
 
 namespace BetterCms.Demo.Web
 {
@@ -12,13 +12,48 @@ namespace BetterCms.Demo.Web
     // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static ICmsHost cmsHost;
         protected void Application_Start()
         {
+            cmsHost = CmsContext.RegisterHost();
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            cmsHost.OnApplicationStart(this);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            cmsHost.OnBeginRequest(this);
+        }
+
+        protected void Application_EndRequest()
+        {
+            cmsHost.OnEndRequest(this);
+        }
+
+        protected void Application_Error()
+        {
+            cmsHost.OnApplicationError(this);
+        }
+
+        protected void Application_End()
+        {
+            cmsHost.OnApplicationEnd(this);
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            var roles = new[] { "BcmsEditContent", 
+                                "BcmsPublishContent", 
+                                "BcmsDeleteContent", 
+                                "BcmsAdministration" };
+            var principal = new GenericPrincipal(new GenericIdentity("TestUser"),
+                                                 roles);
+            HttpContext.Current.User = principal;
+            cmsHost.OnAuthenticateRequest(this);
         }
     }
 }
