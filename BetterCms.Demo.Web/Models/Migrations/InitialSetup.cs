22861206417b90a8b3edceab478b773a72e91a48
Simonas Mikulenas <simonas@devbridge.com>
2013-04-12 13:04:05 +0300

    Add to sitemap, add content infrastructure added.

--- BetterCms.Demo.Web/Models/Migrations/InitialSetup.cs
+++ BetterCms.Demo.Web/Models/Migrations/InitialSetup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using BetterCms.Core.DataContracts.Enums;
 using FluentMigrator;
 
 namespace BetterCms.Demo.Web.Models.Migrations
@@ -7,14 +8,7 @@ namespace BetterCms.Demo.Web.Models.Migrations
     [Migration(201304110949)]
     public class InitialSetup : Migration
     {
-        private enum PageStatus
-        {
-            Preview = 1,
-            Draft = 2,
-            Published = 3,
-            Unpublished = 4
-        }
-
+        private const string demoDataCreationUser = "Admin";
         private const string RootSchemaName = "bcms_root";
         private const string PagesSchemaName = "bcms_pages";
 
@@ -22,6 +16,10 @@ namespace BetterCms.Demo.Web.Models.Migrations
         private const string RegionsTableName = "Regions";
         private const string LayoutRegionsTableName = "LayoutRegions";
         private const string PagesTableName = "Pages";
+        private const string SitemapNodesTableName = "SitemapNodes";
+        private const string ContentsTableName = "Contents";
+        private const string HtmlContentsTableName = "HtmlContents";
+        private const string PageContentsTableName = "PageContents";
 
         private static Guid MainPageTemplateId = new Guid("52EFCC37-5583-46D9-AE06-FA15CA84F286");
         private static Guid MainPageTopRegionId = new Guid("F83A3130-3CC8-4FE1-8585-09969BAA9CDE");
@@ -37,7 +35,7 @@ namespace BetterCms.Demo.Web.Models.Migrations
 //            CreateServerWidgets();
             RemoveDefaultRootPage();
             CreatePages();
-//            CreateSitemap();
+            CreateContent();
         }
 
         public override void Down()
@@ -83,7 +81,7 @@ namespace BetterCms.Demo.Web.Models.Migrations
                     {
                         PageUrl = "/default/",
                         ModifiedOn = DateTime.Now,
-                        ModifiedByUser = "Admin"
+                        ModifiedByUser = demoDataCreationUser
                     })
                 .Where(new { PageUrl = "/" });
         }
@@ -100,6 +98,26 @@ namespace BetterCms.Demo.Web.Models.Migrations
                     MetaTitle = "Ut wisi minim veniam.",
                     MetaKeywords = "doming id quod mazim placerat facer possim assum",
                     MetaDescription = "doming id quod mazim placerat facer possim assum"
+                },
+                new SitemapNode
+                    {
+                        Title = "Home",
+                        Url = "/"
+                    });
+        }
+
+        private void CreateContent()
+        {
+            AddContent(MainPageId, MainPageMainRegionId, 0, new HtmlContent
+                {
+                    Name = "About Us",
+                    Html = @"<h1>About Us</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p><p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis brodi autem vel feugait nulla... <a href='/AboutUs'>Read more About Us</a></p>",
+                });
+
+            AddContent(MainPageId, MainPageMainRegionId, 0, new HtmlContent
+                {
+                    Name = "About Us",
+                    Html = @"<h1>About Us</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p><p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis brodi autem vel feugait nulla... <a href='/AboutUs'>Read more About Us</a></p>",
                 });
         }
 
@@ -131,8 +149,10 @@ namespace BetterCms.Demo.Web.Models.Migrations
             }
         }
 
-        private void AddPage(Page page)
+        private void AddPage(Page page, SitemapNode node = null)
         {
+            page.NodeCountInSitemap += node != null ? 1 : 0;
+
             Insert
                 .IntoTable(PagesTableName)
                 .InSchema(RootSchemaName)
@@ -174,6 +194,66 @@ namespace BetterCms.Demo.Web.Models.Migrations
                         //page.CategoryId,
                         page.NodeCountInSitemap
                     });
+
+            if (node != null)
+            {
+                Insert
+                    .IntoTable(SitemapNodesTableName)
+                    .InSchema(PagesSchemaName)
+                    .Row(node);
+            }
+        }
+
+        private void AddContent(Guid pageId, Guid regionId, int order, HtmlContent content)
+        {
+            Insert
+                .IntoTable(ContentsTableName)
+                .InSchema(RootSchemaName)
+                .Row(new
+                    {
+                        content.Id,
+                        content.Version,
+                        content.IsDeleted,
+                        content.CreatedOn,
+                        content.CreatedByUser,
+                        content.ModifiedOn,
+                        content.ModifiedByUser,
+                        content.Name,
+                        content.Status,
+                        content.PublishedOn,
+                        content.PublishedByUser
+                    });
+
+            Insert
+                .IntoTable(HtmlContentsTableName)
+                .InSchema(PagesSchemaName)
+                .Row(new
+                    {
+                        content.Id,
+                        content.ActivationDate,
+                        content.UseCustomCss,
+                        content.UseCustomJs,
+                        content.Html,
+                        content.EditInSourceMode
+                    });
+
+            Insert
+                .IntoTable(PageContentsTableName)
+                .InSchema(RootSchemaName)
+                .Row(new
+                    {
+                        Id = Guid.NewGuid(),
+                        Version = 1,
+                        IsDeleted = false,
+                        CreatedOn = DateTime.Now,
+                        CreatedByUser = demoDataCreationUser,
+                        ModifiedOn = DateTime.Now,
+                        ModifiedByUser = demoDataCreationUser,
+                        PageId = pageId,
+                        ContentId = content.Id,
+                        RegionId = regionId,
+                        Order = order
+                    });
         }
 
         private abstract class BaseModel
@@ -188,7 +268,7 @@ namespace BetterCms.Demo.Web.Models.Migrations
 
             public BaseModel()
             {
-                ModifiedByUser = CreatedByUser = "Admin";
+                ModifiedByUser = CreatedByUser = demoDataCreationUser;
                 ModifiedOn = CreatedOn = DateTime.Now;
                 IsDeleted = false;
                 Version = 1;
@@ -246,6 +326,46 @@ namespace BetterCms.Demo.Web.Models.Migrations
                 UseNoFollow = false;
                 UseNoIndex = false;
                 NodeCountInSitemap = 0;
+            }
+        }
+
+        private class SitemapNode : BaseModel
+        {
+            public string Title { get; set; }
+            public string Url { get; set; }
+            public int DisplayOrder { get; set; }
+            public Guid? ParentNodeId { get; set; }
+
+            public SitemapNode()
+            {
+                DisplayOrder = 0;
+                ParentNodeId = null;
+            }
+        }
+
+        private class HtmlContent : BaseModel
+        {
+            public string Name { get; set; }
+            public int Status { get; set; }
+            public DateTime PublishedOn { get; set; }
+            public string PublishedByUser { get; set; }
+
+            public DateTime ActivationDate { get; set; }
+            public bool UseCustomCss { get; set; }
+            public bool UseCustomJs { get; set; }
+            public string Html { get; set; }
+            public bool EditInSourceMode { get; set; }
+
+            public HtmlContent()
+            {
+                Status = (int)ContentStatus.Published;
+                PublishedOn = DateTime.Now;
+                PublishedByUser = demoDataCreationUser;
+
+                ActivationDate = DateTime.Now;
+                UseCustomCss = false;
+                UseCustomJs = false;
+                EditInSourceMode = false;
             }
         }
     }
