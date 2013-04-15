ef469bb7f053fad0bacbc0a8cc616fe8f78bfcdf
Simonas Mikulenas <simonas@devbridge.com>
2013-04-15 16:44:51 +0300

    Special handling for main region added.

--- BetterCms.Demo.Web/Models/Migrations/InitialSetup.cs
+++ BetterCms.Demo.Web/Models/Migrations/InitialSetup.cs
@@ -31,9 +31,9 @@ namespace BetterCms.Demo.Web.Models.Migrations
 
         private void AddTemplates()
         {
-            InsertTemplate("Two Columns with Header", "~/Views/Shared/CmsTemplates/_TwoColumnsWithHeader.cshtml", new[] { "Header", "Left", "Right" });
-            InsertTemplate("Two Columns", "~/Views/Shared/CmsTemplates/_TwoColumnsTemplate.cshtml", new[] { "Left", "Right" });
-            InsertTemplate("Three Columns Template", "~/Views/Shared/CmsTemplates/_ThreeColumnsTemplate.cshtml", new[] { "Left", "Middle", "Right" });
+            InsertTemplate("Two Columns with Header", "~/Views/Shared/CmsTemplates/_TwoColumnsWithHeader.cshtml", new[] { "Header", "CMSMainContent", "Right" });
+            InsertTemplate("Two Columns", "~/Views/Shared/CmsTemplates/_TwoColumnsTemplate.cshtml", new[] { "CMSMainContent", "Right" });
+            InsertTemplate("Three Columns Template", "~/Views/Shared/CmsTemplates/_ThreeColumnsTemplate.cshtml", new[] { "CMSMainContent", "Middle", "Right" });
         }
 
         private void AddWidgets()
@@ -54,22 +54,22 @@ namespace BetterCms.Demo.Web.Models.Migrations
         public void AddContent()
         {
             InsertContent("Home", "Header", 0, new Content { Name = "Banner", Html = @"<div class='banner-image'> <div> <hgroup class='banner-text'> <h1>Ut wisi minim veniam</h1> <h2>doming id quod mazim placerat facer possim assum.</h2> </hgroup> <a href='#nolink' class='btn-primary action-arrow'>Call to Action</a> <img src='/Content/images/sample-78.jpg' alt='Banner 1' /> </div> <div> <hgroup class='banner-text'> <h1>Esse molestie consequa</h1> <h2>doming id quod mazim placerat facer possim assum.</h2> </hgroup> <a href='#nolink' class='btn-primary action-arrow'>Call to Action</a> <img src='/Content/images/sample-78.jpg' alt='Banner 2' /> </div> <div> <hgroup class='banner-text'> <h1>Congue nihil imperdiet</h1> <h2>doming id quod mazim placerat facer possim assum.</h2> </hgroup> <a href='#nolink' class='btn-primary action-arrow'>Call to Action</a> <img src='/Content/images/sample-78.jpg' alt='Banner 3' /> </div> <div> <hgroup class='banner-text'> <h1>Ut wisi enim ad minim veniam</h1> <h2>doming id quod mazim placerat facer possim assum. doming id quod mazim placerat facer possim assum</h2> </hgroup> <a href='#nolink' class='btn-primary action-arrow'>Call to Action</a> <img src='/Content/images/sample-78.jpg' alt='Banner 4' /> </div> </div> <div class='banner-nav' id='home-banner'> <a href='#1' class='banner-link link'>Ut wisi enim ad minim veniam</a> <div class='block'> <p> <br /> You've seen it coming! Buy now and get nothing for free! Well, at least no free beer. Perhaps a bear, if you can afford it. </p> </div> <a href='#2' class='banner-link link'>Esse molestie consequa</a> <div class='block'> <p>your bear, you have to admit it! No, we aren't selling bears.</p> </div> <a href='#3' class='banner-link link'>Congue nihil imperdiet</a> <div class='block'> <p>And now, for something completely different. And now, for something completely different. Period.</p> </div> <a href='#4' class='banner-link link'>Ut wisi enim ad minim veniam</a> <div class='block'> <br /> <p>And now, for something completely different. And now.</p> </div> </div> " });
-            InsertContent("Home", "Left", 0, new Content { Name = "About Us", Html = @"<h1>About Us</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p><p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis brodi autem vel feugait nulla... <a href='/AboutUs'>Read more About Us</a></p>" });
+            InsertContent("Home", "CMSMainContent", 0, new Content { Name = "About Us", Html = @"<h1>About Us</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p><p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis brodi autem vel feugait nulla... <a href='/AboutUs'>Read more About Us</a></p>" });
             InsertContent("Home", "Right", 0, new Content { Name = "Gallery", Html = @"<h1>Gallery</h1>" });
 
-            InsertContent("About Us", "Left", 0, new Content { Name = "Lorem ipsum", Html = @"<hgroup> <h1>This is an Example of a Sub Head</h1> <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</h2> </hgroup> <section class='content-image-1'> <img src='/Content/images/sample-45.jpg' alt='Sample 45'> <div class='info-box'> <h2>Consectetuer adipiscing</h2> <h3>doming id quod mazim placerat facer possim assum.</h3> </div> </section> <article class='content-article'> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article> <article class='content-article'> <h2>This is an Example of a Sub Head</h2> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article>" });
+            InsertContent("About Us", "CMSMainContent", 0, new Content { Name = "Lorem ipsum", Html = @"<hgroup> <h1>This is an Example of a Sub Head</h1> <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</h2> </hgroup> <section class='content-image-1'> <img src='/Content/images/sample-45.jpg' alt='Sample 45'> <div class='info-box'> <h2>Consectetuer adipiscing</h2> <h3>doming id quod mazim placerat facer possim assum.</h3> </div> </section> <article class='content-article'> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article> <article class='content-article'> <h2>This is an Example of a Sub Head</h2> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article>" });
             InsertContent("About Us", "Right", 1, "Social Media Links");
             InsertContent("About Us", "Right", 2, "Aside bar");
 
             InsertContent("Blog", "Right", 1, "Social Media Links");
             InsertContent("Blog", "Right", 2, "Aside bar");
 
-            InsertContent("News", "Left", 0, new Content { Name = "Lorem ipsum", Html = @"<hgroup> <h1>This is an Example of a Sub Head</h1> <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</h2> </hgroup>" });
-            InsertContent("News", "Left", 1, new Content { Name = "Lorem ipsum", Html = @"<section class='content-image-2'> <img src='/Content/images/sample-32.jpg' alt='Sample 32'> <div class='info-box'> <h2>Consectetuer adipiscing</h2> <h3>doming id quod mazim placerat facer possim assum.</h3> </div> </section>" });
-            InsertContent("News", "Left", 2, new Content { Name = "Lorem ipsum", Html = @"<article class='content-article'> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article>" });
-            InsertContent("News", "Left", 3, new Content { Name = "Lorem ipsum", Html = @"<article class='content-article'> <h2>This is an Example of a Sub Head</h2> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article>" });
-            InsertContent("News", "Left", 4, new Content { Name = "Lorem ipsum", Html = @"<article class='content-article'> <h2>This is an Example of a Sub Head</h2> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p> <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. </p> <p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p> </article>" });
-            InsertContent("News", "Left", 5, new Content { Name = "Lorem ipsum", Html = @"<section class='quotation'> <h2>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat:</h2> <dl> <dd><span></span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</dd> <dd><span></span>sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna </dd> <dd><span></span>aliquam erat volutpat. Ut wisi enim ad minim veniam</dd> </dl> </section>" });
+            InsertContent("News", "CMSMainContent", 0, new Content { Name = "Lorem ipsum", Html = @"<hgroup> <h1>This is an Example of a Sub Head</h1> <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</h2> </hgroup>" });
+            InsertContent("News", "CMSMainContent", 1, new Content { Name = "Lorem ipsum", Html = @"<section class='content-image-2'> <img src='/Content/images/sample-32.jpg' alt='Sample 32'> <div class='info-box'> <h2>Consectetuer adipiscing</h2> <h3>doming id quod mazim placerat facer possim assum.</h3> </div> </section>" });
+            InsertContent("News", "CMSMainContent", 2, new Content { Name = "Lorem ipsum", Html = @"<article class='content-article'> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article>" });
+            InsertContent("News", "CMSMainContent", 3, new Content { Name = "Lorem ipsum", Html = @"<article class='content-article'> <h2>This is an Example of a Sub Head</h2> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, nos trud exe rci tation ullamc orper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel feugait nulla facilisi.</p> </article>" });
+            InsertContent("News", "CMSMainContent", 4, new Content { Name = "Lorem ipsum", Html = @"<article class='content-article'> <h2>This is an Example of a Sub Head</h2> <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p> <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. </p> <p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p> </article>" });
+            InsertContent("News", "CMSMainContent", 5, new Content { Name = "Lorem ipsum", Html = @"<section class='quotation'> <h2>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat:</h2> <dl> <dd><span></span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</dd> <dd><span></span>sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna </dd> <dd><span></span>aliquam erat volutpat. Ut wisi enim ad minim veniam</dd> </dl> </section>" });
             InsertContent("News", "Right", 1, "Social Media Links");
             InsertContent("News", "Right", 2, "Aside bar");
 
@@ -157,25 +157,35 @@ namespace BetterCms.Demo.Web.Models.Migrations
             foreach (var region in regionNames)
             {
                 var regionId = Guid.NewGuid();
+
                 if (!regions.ContainsKey(region))
                 {
                     // Add region.
-                    regions.Add(region, regionId);
-                    Insert
-                        .IntoTable(Constants.DataBase.Tables.Regions)
-                        .InSchema(Constants.DataBase.Schemas.Root)
-                        .Row(new
-                        {
-                            Version = 1,
-                            IsDeleted = false,
-                            CreatedOn = UpdatedOn,
-                            CreatedByUser = UpdatedBy,
-                            ModifiedOn = UpdatedOn,
-                            ModifiedByUser = UpdatedBy,
+                    if (region == "CMSMainContent")
+                    {
+                        // Special case: BetterCMS contains CMSMainContent region by default.
+                        regionId = new Guid("4161FA8D-299E-460B-8CA1-D27BA455B7B4");
+                    }
+                    else 
+                    {
+                        Insert
+                            .IntoTable(Constants.DataBase.Tables.Regions)
+                            .InSchema(Constants.DataBase.Schemas.Root)
+                            .Row(new
+                            {
+                                Version = 1,
+                                IsDeleted = false,
+                                CreatedOn = UpdatedOn,
+                                CreatedByUser = UpdatedBy,
+                                ModifiedOn = UpdatedOn,
+                                ModifiedByUser = UpdatedBy,
 
-                            Id = regionId,
-                            RegionIdentifier = region,
-                        });
+                                Id = regionId,
+                                RegionIdentifier = region,
+                            });
+                    }
+
+                    regions.Add(region, regionId);
                 }
                 else
                 {
