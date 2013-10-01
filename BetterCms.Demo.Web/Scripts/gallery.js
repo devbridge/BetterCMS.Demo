0b95914fedbdab8070af4e59c0bd2625f31360a6
Audrunas Matonis <audrunas@devbridge.com>
2013-10-01 14:32:52 +0300

    #6 Created images gallery

--- BetterCms.Demo.Web/Scripts/gallery.js
+++ BetterCms.Demo.Web/Scripts/gallery.js
@@ -1,89 +1,59 @@
-function reset() {
-    var replacement = document.createElement('div');
-    replacement.id = 'gallery';
+$(function () {
+    function getQueryParam(param) {
+        var result =  window.location.search.match(
+            new RegExp("(\\?|&)" + param + "(\\[\\])?=([^&]*)")
+        );
 
-    var el = document.getElementById('gallery');
-    el.parentNode.replaceChild(replacement, el);
+        return result ? result[3] : false;
+    }
 
-    window.coverflow('gallery').setup({
-        playlist: [
-            {
-                'title': 'Image 1',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-1.jpg'
-            },
-            {
-                'title': 'Image 2',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-2.jpg'
-            },
-            {
-                'title': 'Image 3',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-3.jpg'
-            },
-            {
-                'title': 'Image 4',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-4.jpg'
-            },
-            {
-                'title': 'Image 5',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-5.jpg'
-            },
-            {
-                'title': 'Image 6',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-6.jpg'
-            },
-            {
-                'title': 'Image 7',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-1.jpg'
-            },
-            {
-                'title': 'Image 8',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-2.jpg'
-            },
-            {
-                'title': 'Image 9',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-3.jpg'
-            },
-            {
-                'title': 'Image 10',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-4.jpg'
-            },
-            {
-                'title': 'Image 11',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-5.jpg'
-            },
-            {
-                'title': 'Image 12',
-                'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed',
-                'image': '/Content/images/gallery-6.jpg'
+    var selectors = {
+        images: '.page-frame .bcms-gallery-image-holder > img',
+        imagesContainer: '.page-frame section:has(.bcms-gallery-image-holder)',
+        galleryContainerId: 'images-gallery-container'
+    },
+        images = [],
+        galleryDiv,
+        imgSrc, imgTitle;
+
+    if (getQueryParam("image-gallery-album-id")) {
+        $(selectors.images).each(function() {
+            imgSrc = $(this).attr('src');
+            imgTitle = $(this).attr('alt');
+            if (imgSrc) {
+                images.push({
+                    title: imgTitle,
+                    description: imgTitle,
+                    image: imgSrc
+                });
             }
-        ],
-        backgroundcolor: 'fff',
-        mode: 'html5',
-        item: 1,
-        width: 940,
-        height: 445,
-        x: 0,
-        y: 0,
-        coverwidth: 525,
-        coverheight: 355,
-        coverangle: 45,
-        covergap: 80,
-        coverdepth: 185,
-        opacitydecrease: 0.5,
-        reflectionratio: 5,
-        fixedsize: true,
-        textoffset: 37
-    });
-}
-//reset();
+        });
+
+        if (images.length > 0) {
+            galleryDiv = $('<div class="gallery-view" id="' + selectors.galleryContainerId + '"></div>');
+
+            $(selectors.imagesContainer).first().replaceWith(galleryDiv);
+            $(selectors.imagesContainer).remove();
+            
+            window.coverflow(selectors.galleryContainerId).setup({
+                playlist: images,
+                backgroundcolor: 'fff',
+                mode: 'html5',
+                item: 0,
+                width: 940,
+                height: 445,
+                x: 0,
+                y: 0,
+                coverwidth: 525,
+                coverheight: 355,
+                coverangle: 45,
+                covergap: 80,
+                coverdepth: 185,
+                opacitydecrease: 0.5,
+                reflectionratio: 5,
+                fixedsize: true,
+                textoffset: 37
+            });
+        }
+    }
+});
