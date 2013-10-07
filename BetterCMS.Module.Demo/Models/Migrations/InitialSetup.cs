e10f618c7e47d42a1605383f3720b6951da3fe45
Audrunas Matonis <audrunas@devbridge.com>
2013-10-07 17:28:55 +0300

    Demo project modifications

--- BetterCMS.Module.Demo/Models/Migrations/InitialSetup.cs
+++ BetterCMS.Module.Demo/Models/Migrations/InitialSetup.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-
+using BetterCms.Configuration;
 using BetterCms.Core.DataAccess.DataContext.Migrations;
 using FluentMigrator;
 
@@ -22,17 +22,39 @@ namespace BetterCMS.Module.Demo.Models.Migrations
             var uri = new UriBuilder(codeBase);
             var path = Uri.UnescapeDataString(uri.Path);
             var rootPath = Path.GetDirectoryName(Path.GetDirectoryName(path));
+            var serverUrl = GetServerUrl();
 
             var script1 = string.Format("UPDATE [bcms_media].[MediaFiles] SET [FileUri] = REPLACE([FileUri], '[WebSiteRootPath]', '{0}')", rootPath);
             IfSqlServer().Execute.Sql(script1);
 
             var script2 = string.Format("UPDATE [bcms_media].[MediaImages] SET [OriginalUri] = REPLACE([OriginalUri], '[WebSiteRootPath]', '{0}'), [ThumbnailUri] = REPLACE([ThumbnailUri], '[WebSiteRootPath]', '{0}')", rootPath);
             IfSqlServer().Execute.Sql(script2);
+
+            var script3 = string.Format("UPDATE [bcms_media].[MediaFiles] SET [PublicUrl] = REPLACE([PublicUrl], '[WebServerPath]', '{0}')", serverUrl);
+            IfSqlServer().Execute.Sql(script3);
+
+            var script4 = string.Format("UPDATE [bcms_media].[MediaImages] SET [PublicOriginallUrl] = REPLACE([PublicOriginallUrl], '[WebServerPath]', '{0}'), [PublicThumbnailUrl] = REPLACE([PublicThumbnailUrl], '[WebServerPath]', '{0}')", serverUrl);
+            IfSqlServer().Execute.Sql(script4);
         }
 
         public override void Down()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        private string GetServerUrl()
+        {
+            var serverUrl = string.Empty;
+
+            var configurationLoader = new DefaultConfigurationLoader();
+            var configuration = configurationLoader.LoadCmsConfiguration();                            
+
+            if (!string.IsNullOrEmpty(configuration.WebSiteUrl) && configuration.WebSiteUrl.ToLower() != "auto")
+            {
+                serverUrl = configuration.WebSiteUrl.ToLower().TrimEnd('/');
+            }
+
+            return serverUrl;
         }
     }
 }
