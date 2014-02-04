353be4b24d809933c7f29851b269e40e3bb68ac9
Simonas Mikulenas <simonas@devbridge.com>
2014-02-04 13:18:54 +0200

    Menu generation updated.

--- BetterCms.Demo.Web/Controllers/SitemapController.cs
+++ BetterCms.Demo.Web/Controllers/SitemapController.cs
@@ -14,11 +14,30 @@ namespace BetterCms.Demo.Web.Controllers
     {
         public virtual ActionResult Index()
         {
-            List<MenuItemViewModel> menuItems;
+            var defaultSitemapId = new Guid("17ABFEE9-5AE6-470C-92E1-C2905036574B");
+
+            var menuItems = new List<MenuItemViewModel>();
 
             using (var api = ApiFactory.Create())
             {
-                menuItems = GetMenuItems(api, new Guid("17ABFEE9-5AE6-470C-92E1-C2905036574B"));
+                var allSitemaps = api.Pages.Sitemap.Get(new GetSitemapsRequest());
+                if (allSitemaps.Data.Items.Count > 0)
+                {
+                    var sitemap = allSitemaps.Data.Items.FirstOrDefault(map => map.Id == defaultSitemapId) ?? allSitemaps.Data.Items.First();
+                    var request = new GetSitemapNodesRequest
+                        {
+                            SitemapId = sitemap.Id
+                        };
+
+                    request.Data.Filter.Add("ParentId", null);
+                    request.Data.Order.Add("DisplayOrder");
+
+                    var response = api.Pages.Sitemap.Nodes.Get(request);
+                    if (response.Data.Items.Count > 0)
+                    {
+                        menuItems = response.Data.Items.Select(mi => new MenuItemViewModel { Caption = mi.Title, Url = mi.Url }).ToList();
+                    }
+                }
             }
 
             return View(menuItems);
@@ -56,32 +75,6 @@ namespace BetterCms.Demo.Web.Controllers
             }
 
             return View(menuItems);            
-        }
-
-        private List<MenuItemViewModel> GetMenuItems(IApiFacade api, Guid sitemapId)
-        {
-            var request = new GetSitemapNodesRequest
-            {
-                SitemapId = new Guid("17ABFEE9-5AE6-470C-92E1-C2905036574B")
-            };
-
-            request.Data.Filter.Add("ParentId", null);
-            request.Data.Order.Add("DisplayOrder");
-
-            var response = api.Pages.Sitemap.Nodes.Get(request);
-            if (response.Data.Items.Count > 0)
-            {
-                return response.Data.Items.Select(mi => new MenuItemViewModel { Caption = mi.Title, Url = mi.Url }).ToList();
-            }
-
-            var allSitemaps = api.Pages.Sitemap.Get(new GetSitemapsRequest());
-            if (allSitemaps.Data.Items.Count > 0)
-            {
-                var sitemap = allSitemaps.Data.Items.First();
-                return GetMenuItems(api, sitemap.Id);
-            }
-
-            return new List<MenuItemViewModel>();
         }
     }
 }
